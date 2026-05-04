89a224c31dbe314fe57a6939a0f6bbe2b25ddeb1
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-05-04 20:11:08 +0530

    on 4th may 20:10

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-name = "Krish"
-Name = "Naik"
-print(name)
-print(Name)
+age = 23
+height = 5.9
+name = "alice"
+is_student = False
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_student))
+
+
+fl_age = float(age)
+print(type(fl_age))
+
+

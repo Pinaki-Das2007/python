3c1a85166c0110f2fcc97df2137dfdaade3260da
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-07-13 21:00:21 +0530

    completed the dictionary class

---
+++ dictonary.py
@@ -0,0 +1,5 @@
+### Dictionaries are unordered collections of key-value pairs. Each key is unique and maps to a specific value. Dictionaries are mutable, meaning you can change their contents after creation. They are defined using curly braces {} with key-value pairs separated by colons.
+
+## Creating a dictionary
+
+empty_dict = {}

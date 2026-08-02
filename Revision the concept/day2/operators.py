5a8907d451a029dfd8c7376b3d50d31090952e93
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-08-02 20:36:14 +0530

    completed the operator part

---
+++ Revision the concept/day2/operators.py
@@ -0,0 +1,45 @@
+# Arithmatic operators
+
+a = 23
+
+b = 45
+
+c = a + b
+print(c)
+
+
+# Assignment operators
+a = 4-2
+b = 6
+# b =+3
+b-=3
+print(b)
+
+
+# Comparison operators
+
+d = 5<=4
+print(d)
+
+f = 5>=5
+print(f)
+
+g = 5==5
+print(g)
+
+
+
+# Logical operators
+# and
+print("Comparison of logical operators")
+
+h = True and False
+print(h)
+
+i = True and True
+print(i)
+j = False and False
+print(j)
+
+print(not (False))
+print(not (True))

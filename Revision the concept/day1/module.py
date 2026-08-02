752cfde945111c4c4c23ad1e537b34c6d50d163d
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-08-02 11:55:30 +0530

    Entered to the day 2

--- Revision the concept/day1/module.py
+++ Revision the concept/day1/module.py
@@ -4,4 +4,14 @@ import pyjokes
 print("Printin Jokes.............")
 joke = pyjokes.get_joke()
 print(joke)
-# print 
+# this is a message from the first module
+
+""" 
+multiline comment 
+"""
+
+
+"""
+this modules are used for basics of python concept
+"""
+

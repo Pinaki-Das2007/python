9f29c1c4d9b822134a61d519524e6f75ba24659f
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-08-02 21:13:04 +0530

    completed the type and typecasting part

---
+++ Revision the concept/day2/type&typecasting.py
@@ -0,0 +1,29 @@
+a = 31
+t = type(a)
+
+print(t)
+
+b= 32.2
+print(type(b))
+
+c = "Harry "
+print(type(c))
+
+d = True
+print(type(d))
+
+e = "32.2"
+print(type(e))
+
+
+## Type casting
+
+a1 = 34
+print(type(a1))
+
+b1 = float(a1)
+print(type(b1))
+
+print(b1)
+
+

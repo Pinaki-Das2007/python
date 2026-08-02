0e0edff464ac62913d33080bd731befacf235fe6
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-08-02 12:06:53 +0530

    updated the file

---
+++ Revision the concept/day2/2ndmodule.py
@@ -0,0 +1,26 @@
+## variables and datatypes
+
+# VARIABlES are containers for storing data values. In Python, variables do not need to be declared with any particular type and can even change type after they have been set.
+
+
+a = 13
+print(a)
+print(type(a))
+
+b = "Pinki"
+print(b)
+print(type(b))
+
+c = 3.14
+print(c)
+print(type(c))
+
+d = True 
+print(d)
+print(type(d))
+
+e= None
+print(e)
+print(type(e))
+
+
---
+++ Revision the concept/day2/rules_variables.py
@@ -0,0 +1,9 @@
+a = 23
+
+aaa = 34
+
+harry = 45
+
+_pinaki = 56
+
+# @pinaki = 34

106058500d7ec9d7d49e3994dd3e25a9bb6930ea
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-08-03 10:52:13 +0530

    completed the string funtion

---
+++ Revision the concept/day3/strfunction.py
@@ -0,0 +1,9 @@
+name = "harrypotter" 
+
+print(len(name))
+print(name.endswith("potter"))
+print(name.startswith("harry"))
+print(name.capitalize())
+
+c = "harry is a good boy \n he is teaching us python"
+print(c)

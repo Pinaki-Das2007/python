10217525cb2f0470e81520eae48596b3321a3cc6
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-08-06 20:04:14 +0530

    completed the set part

---
+++ Revision the concept/day4/sets.py
@@ -0,0 +1,15 @@
+s = {1,2,3,4,5,5,5,6,6,6}
+print(s)
+print(type(s))
+
+
+e = set() ## Never use s = {} as it will create an empty dictionary
+print(type(e))
+
+## Methods in set
+
+print(s, type(s))
+
+s.add(566)
+print(s, type(s))
+

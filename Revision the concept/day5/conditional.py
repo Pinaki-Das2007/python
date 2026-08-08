d396efdb2c8413a2aa91665a157b54e7e1a99972
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-08-08 20:18:11 +0530

    completed the conditional part

---
+++ Revision the concept/day5/conditional.py
@@ -0,0 +1,16 @@
+## If else , elif , nested if else
+
+a = int(input("Enter your age:"))
+if (a >= 18):
+    print("You are eligible to vote")
+    print("Vote for BJP")
+
+elif (a < 0):
+    print("You are entering an invalid age")
+    print("Pagal hey kya BKl!")
+
+else:
+    print("You are not eligible to vote")
+    print("Still vote for BJP")
+
+print("Thanks for voting . Ayega toh MODI HI !")

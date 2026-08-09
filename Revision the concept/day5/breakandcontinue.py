4132698ae0b35c6aa729d38a42de2ca08aedee3c
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-08-09 10:33:13 +0530

    complted the day 5 with the loops concept

---
+++ Revision the concept/day5/breakandcontinue.py
@@ -0,0 +1,14 @@
+for i in range(100):
+    if i == 44 :
+        print("The loop us about to break at 44 .")
+        break
+    print(i)
+
+for i in range(100):
+    pass
+
+
+
+i = 0
+while i < 100:
+    print("Hello world")
---
+++ Revision the concept/day5/loops.py
@@ -0,0 +1,30 @@
+## While loops
+lst = [1, 2, 3, 4, 5]
+i = 0
+while i < len(lst):
+    print(lst[i])
+    i += 1
+
+
+
+## For loops
+
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in  l:
+    print(i)
+    i += 1
+
+
+s = "Hello-world"
+for i in s:
+    print(i)
+
+
+## For loop with else
+
+l = [1, 2, 3, 4, 5]
+for i in l :
+    print(i)
+
+else:
+    print ("The loop is over!")

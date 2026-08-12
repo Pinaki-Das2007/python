d30cbe784209c5de08341ff64616040446cc4232
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-08-12 20:52:46 +0530

    completed the iterators concepts

--- Revision the concept/day8/oops.py
+++ Revision the concept/day8/oops.py
@@ -4,8 +4,7 @@ class employee :
     salary = 50000
 
 harry = employee()
-print(harry.language,harry.salary)
+print(harry.name,harry.salary)
 
-
-
-# Here name is object attribute and salary is class attribute. If we try to access the class attribute using object then it will give us the value of class attribute. But if we try to access the object attribute using class then it will give us the value of object attribute.
+# Here name is object attribute and salary is class attribute. If we try to access the class attribute using object then  it will give us the value of class
+#  attribute. But if we try to access the object attribute using class then it will give us the value of object attribute.

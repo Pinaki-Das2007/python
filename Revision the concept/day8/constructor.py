dacf18c8e2ee912febb4f0f901f9993694f6dd30
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-08-13 19:50:26 +0530

    modified

--- Revision the concept/day8/constructor.py
+++ Revision the concept/day8/constructor.py
@@ -2,7 +2,21 @@ class employee:
     language = "python"
     salary = 1200000
 
-    def __init__(self):
-        pass
+    def __init__ (self,name,language,salary): ## Dunder method which is called automatically when an object is created
+        self.name = name
+        self.language = language
+        self.salary = salary
+        print("I am creating an object")
+
+    def getInfo(self):
+        print(f"The language is {self.language}. The salary is {self.salary}.")
+
+    @staticmethod
+    def greet():
+        print("Good morning")
+
+harry = employee("Harry", "javascript", 1900000)
+# harry.name = "harry"
+print(harry.name,harry.salary,harry.language)
 
     

d0f95baf116145ad6e2d7eb6efcaf3920784f25a
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-08-15 21:49:55 +0530

    property decorator part is end

---
+++ Revision the concept/day11/property_decorators.py
@@ -0,0 +1,28 @@
+class employee:
+    a = 1
+
+    @classmethod
+    def show(cls):
+        print(f"The class attribute of a is {cls.a}")
+
+    @property
+    def name(self):
+        return ""
+
+    @name.setter
+    def name(self,value):
+        self.fname = value.split(" ")[0]
+        self.lname = value.split(" ")[1]
+    
+
+
+e = employee()
+e.a = 45
+
+
+e.name = "John"
+print(e.name)
+
+
+e.show()
+

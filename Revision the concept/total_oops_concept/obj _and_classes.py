9683accb370b7fb06dfcf510fe4b1fabb2b5735f
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-08-16 21:14:40 +0530

    more deep down into oops concept

---
+++ Revision the concept/total_oops_concept/obj _and_classes.py
@@ -0,0 +1,15 @@
+class Student:
+    def __init__ (self,name,rollno):
+        self.name = name
+        self.rollno = rollno
+
+        print("Adding a new student into the database")
+
+   
+
+
+s1 = Student("karan",34)
+print(s1.name,s1.rollno)
+
+s2= Student("arjun",45)
+print(s2.name,s2.rollno)

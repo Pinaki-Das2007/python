d3881804ee55472f50cd020a908c2e63deb81d95
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-08-17 20:39:13 +0530

    the class and instance part completed

--- Revision the concept/total_oops_concept/class&instance.py
+++ Revision the concept/total_oops_concept/class&instance.py
@@ -1,4 +1,8 @@
 class Student:
+    college_name = "GITA AUTONOMOUS COLLEGE"
+    
+
+
     def __init__(self,name,marks):
         self.name = name
         self.marks = marks
@@ -10,3 +14,7 @@ print(s1.name, s1.marks)
 
 s2 = Student("Arjun",88)
 print(s2.name, s2.marks)
+
+print(s1.college_name)
+print(s2.college_name)
+print(Student.college_name)

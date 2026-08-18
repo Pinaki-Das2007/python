38c732107a376cc258cb5a2ac6562408ac08dfa1
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-08-18 10:47:48 +0530

    del keyword concept completed

---
+++ Revision the concept/total_oops_concept/delkeyowrd.py
@@ -0,0 +1,13 @@
+#  Del Keyword in python
+#  Used to delete object properties or object itself.
+
+class Student:
+    def __init__(self,name):
+        self.name = name
+
+s1 = Student("Shradha")
+print(s1.name)
+
+
+del s1.name
+print(s1.name)

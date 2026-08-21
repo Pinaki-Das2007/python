4ac90beb6d599e6b9af784c87b591694a8176b19
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-08-21 20:55:43 +0530

    starting the 2d array

---
+++ numpy/starting.py
@@ -0,0 +1,14 @@
+python_list = [1, 2, 3, 4, 5]
+print(python_list)
+
+
+import numpy as np
+numpy_array = np.array([1,2,3,4,5])
+print(numpy_array)
+
+
+arr_2d = np.array([[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, 9]])
+
+print(arr_2d)

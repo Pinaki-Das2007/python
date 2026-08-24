2585bbc2ca9c981b7a7538ba2463ba2e7f4aee8b
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-08-24 11:27:54 +0530

    aggreation function completed

---
+++ numpy/property/aggergation.py
@@ -0,0 +1,13 @@
+#  taking multiple values and summerizing them into a single value is called aggregation.
+
+
+import numpy as np
+arr = np.array([10,20,30,40,50])
+
+print(np.sum(arr))
+print(np.mean(arr))
+print(np.min(arr))
+print(np.max(arr))
+print(np.std(arr))
+print(np.var(arr))
+print(np.median(arr))

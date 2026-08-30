31c554db22911d201199dbf316b73110057ff5af
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-08-30 20:59:54 +0530

    learned how to save all datas in it's file format

---
+++ pandas/save.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+data = {
+    "Name":["Ram","Shyam","Gopal"],
+    "Age":[10,20,30],
+    "City":["Nagpur","Mumbai","Delhi"]
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+
+# df.to_csv("output.csv",index= False)
+# df.to_excel("Excel_output.xlsx",index=False)
+df.to_json("Json_output.json",orient="records")

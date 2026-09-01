53575200d79986ad07520a0016ec1c83e863f619
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-09-01 20:40:56 +0530

    rows filterations completed

---
+++ pandas/rows_filter.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+data = {
+    "Name": ['Ram', 'Shyam', 'Ghanshyam','Dhansyam','Aditi','Jagdish','Raj','Simran'],
+    "Age": [28,34,22,30,29,40,25,32],
+    "Salary": [50000, 60000, 45000, 52000, 49000, 70000,48000,58000],
+    "Performance Score":[85,90,78,92,88,95,80,89]
+}
+
+
+df = pd.DataFrame(data)
+
+print("Sample Data Frame:")
+print(df)
+
+high_salary= df[df["Salary"]>50000]
+print("Employees with salary greater than 50000:")
+print(high_salary)
+
+
+
+#  filtering based on multiple conditions
+
+filtered = df[(df["Age"]>30) & (df["Salary"]>50000)]
+print(filtered)
+
+
+# using or conditions
+
+filtered_or = df[(df["Age"]>35) | (df["Performance Score"]>50000)]
+print(f"Employees with age greater than 35 or performance score greater than 50000 : \n{filtered_or}")

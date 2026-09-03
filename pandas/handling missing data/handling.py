a5ebd20fcf14aa54ef78e6c64af3c90d84e8401d
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-09-03 21:14:39 +0530

    updated

--- pandas/handling missing data/handling.py
+++ pandas/handling missing data/handling.py
@@ -13,5 +13,7 @@ data = {
 
 df = pd.DataFrame(data)
 print(df)
-df.dropna( inplace=True)  # drop rows with missing values
+# df.dropna( inplace=True)  # drop rows with missing values
+df["Age"].fillna(df["Age"].mean(), inplace=True)  # fill missing values with mean
+df["Salary"].fillna(df["Salary"].mean(), inplace=True)  # fill missing values with mean
 print(df)
